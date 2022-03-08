e6d6de9643027695da89047db7701a1d2cf7756f
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-08 19:55:55 +0530

    Fixed multiple rendered results

--- src/modules/api/fetch.js
+++ src/modules/api/fetch.js
@@ -1,24 +1,32 @@
 // import baseUrl from './base-url';
-import axios from 'axios';
+// import axios from 'axios';
 
-const baseUrl = axios.create({
-	baseURL: 'https://pokeapi.co/api/v2/pokemon',
-});
+// let baseUrl = axios.create({
+// 	baseURL: 'https://pokeapi.co/api/v2/pokemon',
+// });
 
-const getPokemonList = async (offset, limit = 20) => {
-	const res = await baseUrl.get(`?offset${offset}=&limit=20`);
-	console.log(res.data);
-	return res.data;
+const getPokemonList = async offset => {
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`);
+	const data = await res.json();
+	return data;
+
+	// const res = await baseUrl.get(`?offset${offset}=&limit=20`);
 };
 
 const getPokemon = async name => {
-	const res = await baseUrl.get(`/${name}`);
-	return res.data;
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+	const data = await res.json();
+	return data;
+	// const res = await baseUrl.get(`/${name}`);
+	// return res.data;
 };
 
 const getPokemonByHabitat = async habitat => {
-	const res = await baseUrl.get(`-habitat/${habitat}`);
-	return res.data;
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon-habitat/${habitat}`);
+	const data = await res.json();
+	return data;
+	// const res = await baseUrl.get(`-habitat/${habitat}`);
+	// return res.data;
 };
 
 export { getPokemonList, getPokemon, getPokemonByHabitat };
